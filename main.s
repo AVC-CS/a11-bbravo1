8ad22949740139d5d84369fc1426bc79a304f7bc
Brian <bbravo9270@gmail.com>
2026-02-27 15:16:35 -0800

    completed all compilation stages

--- main.s
+++ main.s
@@ -1,10 +1,6 @@
 	.file	"main.cpp"
 	.text
 	.section .rdata,"dr"
-_ZStL19piecewise_construct:
-	.space 1
-.lcomm _ZStL8__ioinit,1,1
-	.def	__main;	.scl	2;	.type	32;	.endef
 .LC0:
 	.ascii "Hello world\0"
 	.text
@@ -12,7 +8,7 @@ _ZStL19piecewise_construct:
 	.def	main;	.scl	2;	.type	32;	.endef
 	.seh_proc	main
 main:
-.LFB1632:
+.LFB2214:
 	pushq	%rbp
 	.seh_pushreg	%rbp
 	movq	%rsp, %rbp
@@ -21,90 +17,31 @@ main:
 	.seh_stackalloc	32
 	.seh_endprologue
 	call	__main
-	leaq	.LC0(%rip), %rdx
-	movq	.refptr._ZSt4cout(%rip), %rcx
-	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
-	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rdx
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdx
+	movq	.refptr._ZSt4cout(%rip), %rax
 	movq	%rax, %rcx
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc
+	movq	%rax, %rcx
+	movq	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_(%rip), %rax
+	movq	%rax, %rdx
 	call	_ZNSolsEPFRSoS_E
 	movl	$0, %eax
 	addq	$32, %rsp
 	popq	%rbp
 	ret
 	.seh_endproc
-	.def	__tcf_0;	.scl	3;	.type	32;	.endef
-	.seh_proc	__tcf_0
-__tcf_0:
-.LFB2134:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$32, %rsp
-	.seh_stackalloc	32
-	.seh_endprologue
-	leaq	_ZStL8__ioinit(%rip), %rcx
-	call	_ZNSt8ios_base4InitD1Ev
-	nop
-	addq	$32, %rsp
-	popq	%rbp
-	ret
-	.seh_endproc
-	.def	_Z41__static_initialization_and_destruction_0ii;	.scl	3;	.type	32;	.endef
-	.seh_proc	_Z41__static_initialization_and_destruction_0ii
-_Z41__static_initialization_and_destruction_0ii:
-.LFB2133:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$32, %rsp
-	.seh_stackalloc	32
-	.seh_endprologue
-	movl	%ecx, 16(%rbp)
-	movl	%edx, 24(%rbp)
-	cmpl	$1, 16(%rbp)
-	jne	.L6
-	cmpl	$65535, 24(%rbp)
-	jne	.L6
-	leaq	_ZStL8__ioinit(%rip), %rcx
-	call	_ZNSt8ios_base4InitC1Ev
-	leaq	__tcf_0(%rip), %rcx
-	call	atexit
-.L6:
-	nop
-	addq	$32, %rsp
-	popq	%rbp
-	ret
-	.seh_endproc
-	.def	_GLOBAL__sub_I_main;	.scl	3;	.type	32;	.endef
-	.seh_proc	_GLOBAL__sub_I_main
-_GLOBAL__sub_I_main:
-.LFB2135:
-	pushq	%rbp
-	.seh_pushreg	%rbp
-	movq	%rsp, %rbp
-	.seh_setframe	%rbp, 0
-	subq	$32, %rsp
-	.seh_stackalloc	32
-	.seh_endprologue
-	movl	$65535, %edx
-	movl	$1, %ecx
-	call	_Z41__static_initialization_and_destruction_0ii
-	nop
-	addq	$32, %rsp
-	popq	%rbp
-	ret
-	.seh_endproc
-	.section	.ctors,"w"
-	.align 8
-	.quad	_GLOBAL__sub_I_main
-	.ident	"GCC: (tdm64-1) 10.3.0"
+	.section .rdata,"dr"
+_ZNSt8__detail30__integer_to_chars_is_unsignedIjEE:
+	.byte	1
+_ZNSt8__detail30__integer_to_chars_is_unsignedImEE:
+	.byte	1
+_ZNSt8__detail30__integer_to_chars_is_unsignedIyEE:
+	.byte	1
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.ident	"GCC: (Rev2, Built by MSYS2 project) 14.2.0"
 	.def	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc;	.scl	2;	.type	32;	.endef
 	.def	_ZNSolsEPFRSoS_E;	.scl	2;	.type	32;	.endef
-	.def	_ZNSt8ios_base4InitD1Ev;	.scl	2;	.type	32;	.endef
-	.def	_ZNSt8ios_base4InitC1Ev;	.scl	2;	.type	32;	.endef
-	.def	atexit;	.scl	2;	.type	32;	.endef
 	.section	.rdata$.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_, "dr"
 	.globl	.refptr._ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_
 	.linkonce	discard
